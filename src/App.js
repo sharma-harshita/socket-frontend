6a43991dd806c4b07b43fc23f7803b17fb641464
harshita.sharma <harshita.sharma@asksid.ai>
2021-12-16 17:58:16 +0530

    jhajs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+import {io} from "socket.io-client"
+class App extends React.Component{
+  constructor(){
+    super();
+    // this.socket = io("http://localhost:3001")
+    this.state={
+      msg : ""
+    }
+  }
+  handleChange =(event)=>{
+    this.setState({
+      msg:event.target.value
+    })
+  }
+  handleSubmit = ()=>{
+    var socket = io("http://localhost:3001");
+    socket.emit('chat message', this.state.msg);
+    socket.on('chat message', (value)=>{
+      console.log(value);
+    })
+  }
+  render(){
+    return(
+      <div>Hello
+        <input type="text" id="msg" name="msg" onChange={this.handleChange} value={this.state.msg}/>
+        <button onClick={this.handleSubmit}>Submit</button>
+      </div>
+      
+    )
+  }
 }
 
-export default App;
+export default App
